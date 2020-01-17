24fbf33e7a73b09fbb2b4fafbdf632204f56f901
Istvan Novak <dotneteer@hotmail.com>
2020-01-17 07:05:19 +0100

    Typescript compilation set up

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+  import Titlebar from "./shell/Titlebar.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    flex-shrink: 0;
+	user-select: none;
+	background-color: #202020
+  }
 </style>
+
+<main>
+  <Titlebar />
+</main>
---
+++ src/shell/Titlebar.svelte
@@ -0,0 +1,33 @@
+<script>
+  import {a} from "../tslib/themes/hello"
+</script>
+
+<style>
+  .component {
+    display: flex;
+    flex-shrink: 0;
+    height: 30px;
+    color: white;
+    box-sizing: border-box;
+    align-items: center;
+    justify-content: start;
+    font-size: 0.9em;
+    -webkit-app-region: drag;
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 30px;
+    height: 30px;
+    margin: 0px 8px;
+  }
+</style>
+
+<div class="component">
+  <div class="logo">
+    <img alt="logo" src="./assets/spectnet-logo.png" />
+  </div>
+  <p>{a}</p>
+</div>
